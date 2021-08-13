fc77dbe77dd5d4e518bf8077fb42c0febbcf8d46
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-08-13 19:09:42 +0900

    main.py修正

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 
 app = Flask(__name__)
@@ -9,6 +10,15 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
